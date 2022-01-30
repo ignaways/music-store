bd5add950c6702b04d7c107e2e2e4e34bfb978e4
ignatius andri pratama putra <ignaway@gmail.com>
2022-01-30 18:55:33 +0700

    test web 1

---
+++ script.js
@@ -0,0 +1,32 @@
+function guitar() {
+    var audio = document.getElementById('guitar');
+        audio.play() 
+}
+function electricGuitar() {
+    var audio = document.getElementById('electricGuitar');
+        audio.play() 
+}
+function piano() {
+    var audio = document.getElementById('piano');
+        audio.play() 
+}
+function violin() {
+    var audio = document.getElementById('violin');
+        audio.play() 
+}
+function bass() {
+    var audio = document.getElementById('bass');
+        audio.play() 
+}
+function drum() {
+    var audio = document.getElementById('drum');
+        audio.play() 
+}
+function ukulele() {
+    var audio = document.getElementById('ukulele');
+        audio.play() 
+}
+function trumpet() {
+    var audio = document.getElementById('trumpet');
+        audio.play() 
+}
